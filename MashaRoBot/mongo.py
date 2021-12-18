fdfec6e0ac2098d4f30922613b0ef2a27255a923
Tushar Gadhe <80459967+attitudeking1@users.noreply.github.com>
2021-12-19 00:39:21 +0530

    Update mongo.py

--- MashaRoBot/mongo.py
+++ MashaRoBot/mongo.py
@@ -6,7 +6,7 @@ from MashaRoBot import MONGO_DB_URI
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
 from MashaRoBot.conf import get_int_key, get_str_key
-
+from MashaRoBot import LOGGER
 
 MONGO_PORT = get_int_key("27017")
 MONGO_DB_URI = get_str_key("MONGO_DB_URI")
@@ -21,4 +21,4 @@ db = client["MashaRoBot"]
 try:
     asyncio.get_event_loop().run_until_complete(motor.server_info())
 except ServerSelectionTimeoutError:
-    sys.exit(log.critical("Can't connect to mongodb! Exiting..."))
+    sys.exit(LOGGER.critical("Can't connect to mongodb! Exiting..."))
