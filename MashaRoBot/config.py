59466afd9659b26c22b0d20d6b22f6a4faff9296
Tushar Gadhe <80459967+attitudeking1@users.noreply.github.com>
2021-12-19 02:12:53 +0530

    Update config.py

--- MashaRoBot/config.py
+++ MashaRoBot/config.py
@@ -38,7 +38,7 @@ class Config(object):
     WEBHOOK = False
     INFOPIC = True
     URL = None
-    TEMP_DOWNLOAD_DIRECTORY = None
+    TEMP_DOWNLOAD_DIRECTORY = ""
     # OPTIONAL
     ##List of id's -  (not usernames) for users which have sudo access to the bot.
     DRAGONS = "1378449566 2097082001 2072357336 2136080200 1926801217 936481432"
