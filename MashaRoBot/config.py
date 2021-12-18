2471e5ac8adc69944c76dfb9e8920db212fd1980
Tushar Gadhe <80459967+attitudeking1@users.noreply.github.com>
2021-12-19 01:50:56 +0530

    Update config.py

--- MashaRoBot/config.py
+++ MashaRoBot/config.py
@@ -54,6 +54,7 @@ class Config(object):
     PORT = 5000
     DEL_CMDS = True  # Delete commands that users dont have access to, like delete /ban if a non admin uses it.
     STRICT_GBAN = True
+    STRICT_GMUTE = True
     WORKERS = (
         8  # Number of subthreads to use. Set as number of threads your processor uses
     )
