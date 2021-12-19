b040515924a13e4610e25abb8aefa457353a9283
Tushar Gadhe <80459967+attitudeking1@users.noreply.github.com>
2021-12-19 10:38:37 +0530

    Update afk_sql.py

--- MashaRoBot/modules/sql/afk_sql.py
+++ MashaRoBot/modules/sql/afk_sql.py
@@ -1,6 +1,6 @@
 import threading
 
-from Zaid.modules.sql import BASE, SESSION
+from MashaRoBot.modules.sql import BASE, SESSION
 from sqlalchemy import Boolean, Column, Integer, UnicodeText
 from sqlalchemy.sql.sqltypes import BigInteger
 
