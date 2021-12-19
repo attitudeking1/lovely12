6019dcd8d255397dc0ec876906bdca7f22bf6287
Tushar Gadhe <80459967+attitudeking1@users.noreply.github.com>
2021-12-19 10:44:13 +0530

    Update forceSubscribe_sql.py

--- MashaRoBot/modules/sql/forceSubscribe_sql.py
+++ MashaRoBot/modules/sql/forceSubscribe_sql.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, String, Numeric, Boolean
-from Zaid.modules.sql import BASE, SESSION
+from MashaRoBot.modules.sql import BASE, SESSION
 
 
 class forceSubscribe(BASE):
