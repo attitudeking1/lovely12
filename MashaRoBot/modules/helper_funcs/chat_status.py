9fd58a4a3f0558801316a0c1d8c2e550c60cab1c
Tushar Gadhe <80459967+attitudeking1@users.noreply.github.com>
2021-12-19 13:56:39 +0530

    Update chat_status.py

--- MashaRoBot/modules/helper_funcs/chat_status.py
+++ MashaRoBot/modules/helper_funcs/chat_status.py
@@ -396,7 +396,6 @@ def user_can_ban(func):
         if (
             not member.can_restrict_members
             and member.status != "creator"
-            and user not in DRAGONS
             and user not in [777000, 1087968824]
         ):
             update.effective_message.reply_text(
