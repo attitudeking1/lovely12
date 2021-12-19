04bcb78791e1e53956c51da313b1371d231d6693
Tushar Gadhe <80459967+attitudeking1@users.noreply.github.com>
2021-12-19 10:41:45 +0530

    Update blsticker_sql.py

--- MashaRoBot/modules/sql/blsticker_sql.py
+++ MashaRoBot/modules/sql/blsticker_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText, distinct, func, Integer
 
-from Zaid.modules.sql import BASE, SESSION
+from MashaRoBot.modules.sql import BASE, SESSION
 
 
 class StickersFilters(BASE):
